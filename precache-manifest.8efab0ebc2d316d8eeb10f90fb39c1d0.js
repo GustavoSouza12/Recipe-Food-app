0ec5981920e5077fa42ff8ec2f26e212cd1b7b10
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-08-18 06:36:12 -0300

    Updates

---
+++ precache-manifest.8efab0ebc2d316d8eeb10f90fb39c1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12314960c36d85be4e02170add652cf",
+    "url": "/Recipe-Food-app/index.html"
+  },
+  {
+    "revision": "477040cdb8e60353bd2a",
+    "url": "/Recipe-Food-app/static/css/main.511f94c0.chunk.css"
+  },
+  {
+    "revision": "057e27aa85472a32d76e",
+    "url": "/Recipe-Food-app/static/js/2.c62d57ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Food-app/static/js/2.c62d57ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477040cdb8e60353bd2a",
+    "url": "/Recipe-Food-app/static/js/main.52a1e65b.chunk.js"
+  },
+  {
+    "revision": "b8f6e09c4006cedb2c21",
+    "url": "/Recipe-Food-app/static/js/runtime-main.ed92590f.js"
+  }
+]);
